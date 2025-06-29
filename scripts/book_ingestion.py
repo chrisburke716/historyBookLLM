2017fd046ae793f457e7fc5294faf25234f56706
Christopher Burke <imcjb8@gmail.com>
2025-06-28 21:01:59 -0400

    book ingestion script: remove old code to write to database

--- scripts/book_ingestion.py
+++ scripts/book_ingestion.py
@@ -4,21 +4,13 @@
 # from history_book.data_models.book import BookDBModel, ChapterDBModel, ParagraphDBModel
 from history_book.data_ingestion.book_ingestion import build_history_book_db
 
+"""
+This script runs the full ingestion process for the history book.
+It ingests the book text into book, chapter, and paragraph collections, 
+and writes those to the database and generates embeddings for paragraphs.
+"""
+
 if __name__ == "__main__":
     # Get book data (books, paragraphs, chapters)
-    all_books, all_chapters, all_paragraphs = build_history_book_db()
-    # TODO: fix - first page of every chapter is read twice: into previous chapter and into current chapter
-
-    print("Ingesting history book data into the database...")
-    # Write all models to their respective collections
-    print("\tWriting books to collection...")
-    for book in all_books:
-        book.write_model_to_collection()
-    print("\tWriting chapters to collection...")
-    for chapter in all_chapters:
-        chapter.write_model_to_collection()
-    print("\tWriting paragraphs to collection...")
-    for paragraph in all_paragraphs:
-        paragraph.write_model_to_collection()
-
+    build_history_book_db()
     print("History book data has been successfully ingested into the database.")
